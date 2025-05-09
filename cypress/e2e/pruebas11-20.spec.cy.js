28299f636749433b812237a19b03087578c5873a
Carlos Alvarez <129915737+cear-dev@users.noreply.github.com>
2025-05-08 18:18:38 -0600

    Agrega IDs Cypress: pruebas11-30

--- cypress/e2e/pruebas11-20.spec.cy.js
+++ cypress/e2e/pruebas11-20.spec.cy.js
@@ -1,64 +1,64 @@
 describe('Test Cases del 011 al 020', () => {
-    it('Acceder a Somos la salle bajio', () => {
+    it('TC_SOMOS_lasallebajio_011 - Acceder a Somos la salle bajio', () => {
         cy.visit('https://www.lasallebajio.edu.mx/');
         cy.contains('span', 'la salle bajío').click();
         cy.contains('span', 'somos').should('exist');
       });
 
-      it('Acceder a Servicios a la comunidad', () => {
+      it('TC_ SERVICIOSa la comunidad _012 - Acceder a Servicios a la comunidad', () => {
         cy.visit('https://www.lasallebajio.edu.mx/');
         cy.contains('span', 'a la comunidad').click();
         cy.contains('span', 'servicios').should('exist');
       });
 
-      it('Acceder a Visita nuestros campus', () => {
+      it('TC_ VISITA nuestros campus_013 - Acceder a Visita nuestros campus', () => {
         cy.visit('https://www.lasallebajio.edu.mx/');
         cy.contains('span', 'nuestros campus').click();
         cy.contains('span', 'visita').should('exist');
       });
 
-      it('Acceder a Apoyos estudiantiles', () => {
+      it('TC_APOYOS_estudiantiles_014 - Acceder a Apoyos estudiantiles', () => {
         cy.visit('https://www.lasallebajio.edu.mx/');
         cy.contains('span', 'estudiantiles').click();
         cy.contains('span', 'apoyos').should('exist');
       });       
 
-      it('Mostrar informacion de Beca por promedio', () => {
+      it('TC_APOYOS_estudiantiles_BecaPromedio_015 - Mostrar informacion de Beca por promedio', () => {
         cy.visit('https://www.lasallebajio.edu.mx/');
         cy.contains('span', 'estudiantiles').click();
         cy.get('a[href="#panel-2"]').click();
         cy.contains('span.panel_title', 'beca por promedio').should('exist');
       });
 
-      it('Mostrar informacion de Beca institucional prepago', () => {
+      it('TC_APOYOS_estudiantiles_BecaPregrado_016 - Mostrar informacion de Beca institucional prepago', () => {
         cy.visit('https://www.lasallebajio.edu.mx/');
         cy.contains('span', 'estudiantiles').click();
         cy.get('a[href="#panel-2"]').click();
         cy.contains('span.panel_title', 'beca institucional pregrado').should('exist');
       });
 
-      it('Mostrar informacion de Beca institucional posgrado', () => {
+      it('TC_APOYOS_estudiantiles_BecaPosgrado_017 - Mostrar informacion de Beca institucional posgrado', () => {
         cy.visit('https://www.lasallebajio.edu.mx/');
         cy.contains('span', 'estudiantiles').click();
         cy.get('a[href="#panel-2"]').click();
         cy.contains('span.panel_title', 'beca institucional posgrado').should('exist');
       });
 
-      it('Mostrar informacion de Beca familiar', () => {
+      it('TC_APOYOS_estudiantiles_BecaFamiliar_018 - Mostrar informacion de Beca familiar', () => {
         cy.visit('https://www.lasallebajio.edu.mx/');
         cy.contains('span', 'estudiantiles').click();
         cy.get('a[href="#panel-2"]').click();
         cy.contains('span.panel_title', 'beca familiar').should('exist');
       });
 
-      it('Mostrar informacion de Beca por mérito deportivo', () => {
+      it('TC_APOYOS_estudiantiles_BecaDeportiva_019 - Mostrar informacion de Beca por mérito deportivo', () => {
         cy.visit('https://www.lasallebajio.edu.mx/');
         cy.contains('span', 'estudiantiles').click();
         cy.get('a[href="#panel-2"]').click();
         cy.contains('span.panel_title', 'beca por mérito deportivo').should('exist');
       });
 
-      it('Mostrar informacion de Beca por defunción', () => {
+      it('TC_APOYOS_estudiantiles_BecaDefuncion_020 - Mostrar informacion de Beca por defunción', () => {
         cy.visit('https://www.lasallebajio.edu.mx/');
         cy.contains('span', 'estudiantiles').click();
         cy.get('a[href="#panel-2"]').click();
--- cypress/e2e/pruebas21-30.spec.cy.js
+++ cypress/e2e/pruebas21-30.spec.cy.js
@@ -1,59 +1,59 @@
 describe('Test Cases del 021 al 030', () => {
-    it('Acceder a Agenda universitaria', () => {
+    it('TC_AGENDAuniversitaria_021 - Acceder a Agenda universitaria', () => {
         cy.visit('https://www.lasallebajio.edu.mx/');
         cy.contains('span', 'universitaria').click();
         cy.contains('strong', 'AGENDA').should('exist');
       });
 
-    it('Acceder a Adminsiones', () => {
+    it('TC_NOTICIAS_023 - TC_ADMISIONES_022 - Acceder a Adminsiones', () => {
         cy.visit('https://www.lasallebajio.edu.mx/');
         cy.contains('strong', 'ADMISIONES').click();
         cy.contains('h1', 'ADMISIONES').should('exist');
       });
 
-    it('Acceder a Noticias', () => {    
+    it('TC_NOTICIAS_023 - Acceder a Noticias', () => {    
         cy.visit('https://www.lasallebajio.edu.mx/');
         cy.contains('strong', 'NOTICIAS').click();
         cy.contains('span', 'buscar noticias').should('exist');
       });
 
-    it('Acceder a International dental program', () => {
+    it('TC_INTERNATIONALDENTAL_024 - Acceder a International dental program', () => {
         cy.visit('https://www.lasallebajio.edu.mx/');
         cy.contains('strong', 'INTERNATIONAL').click();
         cy.contains('div', 'INTERNATIONAL DENTAL PROGRAM').should('exist');
       });
 
-    it('Acceder a MIM', () => {
+    it('TC_MIM_025 - Acceder a MIM', () => {
         cy.visit('https://www.lasallebajio.edu.mx/');
         cy.contains('strong', 'MIM').click();
         cy.contains('span', 'El presente de la tradición. Arte popular guanajuatense').should('exist');
       });
 
-    it('Acceder a Investigación', () => {
+    it('TC_INVESTIGACION_026 - Acceder a Investigación', () => {
         cy.visit('https://www.lasallebajio.edu.mx/');
         cy.contains('strong', 'INVESTIGACIÓN').click();
         cy.contains('span', 'vida en').should('exist');
       });
 
-    it('Acceder a Directorio', () => {
+    it('TC_DIRECTORIO_027 - Acceder a Directorio', () => {
         cy.visit('https://www.lasallebajio.edu.mx/');
         cy.contains('a', 'DIRECTORIO').click();
         cy.contains('span', 'directorio').should('exist');
       });
 
-    it('Acceder a Mapas', () => {
+    it('TC_MAPAS_028 - Acceder a Mapas', () => {
         cy.visit('https://www.lasallebajio.edu.mx/');
         cy.contains('a', 'MAPAS').click();
         cy.contains('a', 'mapas').should('exist');
       });
 
-      it('Acceder a Calendario', () => {
+      it('TC_CALENDARIO_029 - Acceder a Calendario', () => {
         cy.visit('https://www.lasallebajio.edu.mx/');
         cy.get('#cal_link').click();  // Usa el nuevo id "cal_link" para hacer clic
         cy.contains('strong', 'CALENDARIO').should('exist');  // Verifica que el calendario se cargue
       });
 
-      it('Acceder a Información campus campestre', () => {
+      it('TC_CAMPUS_CAMPESTRE_030 - Acceder a Información campus campestre', () => {
         cy.visit('https://www.lasallebajio.edu.mx/');
         cy.contains('a', 'Campestre - León, Gto.').click();
         cy.contains('div', 'CAMPESTRE').should('exist');  // Verifica que la información del campus se cargue
